6949a21b6d26065331ee8f16b4c535bb4e574d29
awestover <alek.westover@gmail.com>
2017-12-21 16:34:17 -0500

    validMove function constructed (pain)

--- functions.py
+++ functions.py
@@ -8,6 +8,15 @@ def validMove(lastState, nextState, mod=5):
     switch = False
     hit = False
 
+    # wrong size!
+    if len(nextState) != 4:
+        return False
+
+    # no negative hands! (by convention)
+    for i in range(0, 4):
+        if nextState[i] < 0:
+            return False
+
     # nothing changed, not valid move
     if lastState == nextState:
         return False
@@ -20,7 +29,17 @@ def validMove(lastState, nextState, mod=5):
             switch = True
 
     # check for a hit, using the modulus
-
+    # you didnt change your hands, so I hope you changed the other persons...
+    if lastState[0] == nextState[0] and lastState[1] == nextState[1]:
+        count = 0
+        for i in range(0, 2):
+            for j in range(2, 4):
+                if (lastState[i] + lastState[j]) % mod == nextState[j]:
+                    count += 1
+        if count > 1:
+            return False
+        elif count == 1:
+            hit = True
 
     if switch and not hit:
         return True
@@ -29,13 +48,23 @@ def validMove(lastState, nextState, mod=5):
     else:
         return False
 
+# parse a string of space seperated values to an array
+def parseState(state):
+    try:
+        state = state.strip().split(" ")
+        state = [int(s) for s in state]
+    except:
+        state = [-1, -1, -1, -1]
+    return state
+
 # get a state from the user
 def inputState(state, mod=5):
     drawState(state)
-    nextMove = input("input a move values are space seperated\t")
-    nextMove = nextMove.split(" ").strip()
-    if validMove()
-
+    nextMove = [-1, -1, -1, -1]
+    while not validMove(nextMove):
+        nextMove = parseState(input("input move values, space seperated\t"))
+    drawState(nextMove)
+    return nextMove
 
 # computer move
 def advanceState(state):
