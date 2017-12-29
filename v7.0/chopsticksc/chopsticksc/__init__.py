907d1d731a17a49534b35d9a1b1aad9d3f94f1d6
awestover <alek.westover@gmail.com>
2017-12-29 15:46:40 -0500

    finds the path that the strategy file is really in

--- v7.0/chopsticksc/chopsticksc/__init__.py
+++ v7.0/chopsticksc/chopsticksc/__init__.py
@@ -134,7 +134,8 @@ def advanceState(state):
 
 # looks up a move in the table, returns all recorder next moves
 def lookUpNextMove(lastMove):
-    strategyFile = "strategy.csv"
+    # iffy path finding 
+    strategyFile = os.path.dirname(__file__) + "strategy.csv"
     try:
         stratCsv = pd.read_csv(strategyFile)
     except:
