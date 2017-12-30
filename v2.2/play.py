e8138216e40ef42256e9adfdeb4f079ab6bbaa25
awestover <alek.westover@gmail.com>
2017-12-30 17:56:57 -0500

    work on recursion

--- v2.2/play.py
+++ v2.2/play.py
@@ -10,14 +10,26 @@ gameOverPointConversion = {
     0: 0
 }
 
+invertScores = {
+    0: 0,
+    -1: 1,
+    1: -1
+}
+
 def scoreMoves(state, maxDepth, mod=5):
     ps = possibleNextMoves(state, mod=mod)
     scores = [0 for i in range(0, len(ps))]
     if maxDepth == 0:
         return gameOverPointConversion[gameOver(state)]
     for p in range(0, len(ps)):
-        cur = evaluateMove(p, maxDepth - 1, mod=mod)
+        cur = propogate_invert_scores(scoreMoves(invertState(p), maxDepth - 1, mod=mod))
         scores[p] = sum(cur) / len(cur)
     return scores
 
-print(scoreMove())
+def propogate_invert_scores(array):
+    out = []
+    for a in array:
+        out.append(invertScores[a])
+    return out
+
+print(scoreMoves([1,1,1,1], 3))
