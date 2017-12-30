95966b8cd3f02107fefb8f487c45564c771f8d73
awestover <alek.westover@gmail.com>
2017-12-30 16:38:51 -0500

    fixing up v1.1

--- v1.1/chopsticks.py
+++ v1.1/chopsticks.py
@@ -1,15 +1,22 @@
 from updatePicture import updatePicture
 from drawState import drawState
-from functions import *
+from speak_functions import *
 import time
 import sys
 import os
+sys.path.insert(0, "..")
+from base_functions import *
+from universal_functions import *
 
-strategyFile = "strategy.csv"
-
-if not os.path.exists(strategyFile):
+# what modulus is the game played in?
+mod = 5
+try:
+    mod = int(input("what modulus would you like to play in? (answer an integer between 2 and 9 inclusive)\t"))
+except:
+    pass
+if not os.path.exists(modFileName(strategyFile, mod=mod)):
     headDf = pd.DataFrame(columns=["Previous", "Next"])
-    headDf.to_csv(strategyFile, index=False)
+    headDf.to_csv(modFileName(strategyFile, mod=mod), index=False)
 
 print("Let's begin")
 
@@ -26,7 +33,7 @@ else:
     player = "computer"
 
 mic = True
-micAns = input("Would you like to use the microphone for your speech input?(y/n)")
+micAns = input("Would you like to use the microphone for your speech input?(y/n)\t")
 if len(micAns) > 0:
     if micAns[0].lower() == 'n':
         mic = False
@@ -37,18 +44,18 @@ while gameOver(state) == -1:
         if player == "computer":
             print("My turn")
             time.sleep(0.5)
-            state = advanceState(state)
-            time.sleep(1)
+            state = advanceState(state, strategyFile, mod=mod)
+            time.sleep(0.5)
         else:
             print("Player turn")
-            state = inputState(state)
+            state = conform(inputState(state, strategyFile, mod=mod))
     else:
         print("Player turn")
         promptMove()
         # note the state assumes the current player is listed first
         state = invertState(state)
         # make the choice
-        state = inputState(state, mic=mic)
+        state = conform(s_inputState(state, strategyFile, mod=mod, mic=mic))
         # we must flip twice
         state = invertState(state)
     turn += 1
