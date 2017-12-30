e7a307fc9d389c804cabd0fad0c04c34429b4cc2
awestover <alek.westover@gmail.com>
2017-12-30 18:05:31 -0500

    this is the recursion delver. We need a better point system though

--- v2.2/play.py
+++ v2.2/play.py
@@ -5,31 +5,30 @@ from universal_functions import *
 
 gameOverPointConversion = {
     -1: 0,
-    2: -1,
-    1: 1,
+    2: 1,
+    1: -1,
     0: 0
-}
-
-invertScores = {
-    0: 0,
-    -1: 1,
-    1: -1
 }
 
 def scoreMoves(state, maxDepth, mod=5):
     ps = possibleNextMoves(state, mod=mod)
     scores = [0 for i in range(0, len(ps))]
-    if maxDepth == 0:
-        return gameOverPointConversion[gameOver(state)]
+
+    if maxDepth == 0 or gameOver(state) != -1:
+        return [gameOverPointConversion[gameOver(state)]], ps
     for p in range(0, len(ps)):
-        cur = propogate_invert_scores(scoreMoves(invertState(p), maxDepth - 1, mod=mod))
+        cur = propogate_invert_scores(scoreMoves(invertState(ps[p]), maxDepth - 1, mod=mod)[0])
         scores[p] = sum(cur) / len(cur)
-    return scores
+
+    return scores, ps
 
 def propogate_invert_scores(array):
     out = []
     for a in array:
-        out.append(invertScores[a])
+        out.append(-1*a)
     return out
 
-print(scoreMoves([1,1,1,1], 3))
+
+state = [0, 4, 1, 1]
+print(state)
+print(scoreMoves(state, 3))
